78790bce6adf9fd908007bb0752b46a3ab631b1f
Rebaona Radebe <crebaona@gmail.com>
2026-04-17 22:23:39 +0200

    Initial commit of BooksWebApp

---
+++ build/generated-sources/ap-source-output/za/ac/tut/model/entity/Book_.java
@@ -0,0 +1,21 @@
+package za.ac.tut.model.entity;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2026-04-12T17:38:45")
+@StaticMetamodel(Book.class)
+public class Book_ { 
+
+    public static volatile SingularAttribute<Book, Boolean> illustrations;
+    public static volatile SingularAttribute<Book, Integer> numOfPages;
+    public static volatile SingularAttribute<Book, Double> price;
+    public static volatile SingularAttribute<Book, String> isbn;
+    public static volatile SingularAttribute<Book, String> description;
+    public static volatile SingularAttribute<Book, Long> id;
+    public static volatile SingularAttribute<Book, String> title;
+    public static volatile SingularAttribute<Book, Date> creationDate;
+
+}
---
+++ build/web/add_book_outcome.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : add_book_outcome
+    Created on : Apr 12, 2026, 2:18:02 PM
+    Author     : Rebaona
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Add Book Outcome page</title>
+    </head>
+    <body>
+        <h1>Add Book Outcome</h1>
+        <p>
+            The book has been successfully added.
+        </p>
+        
+        <ol>
+            <li><a href="menu.html">Menu Page</a></li>
+            <li><a href="index.html">Home Page</a></li>
+        </ol>
+    </body>
+</html>
